ae2bc302f5cd26c832b2f230219893fffb001c74
harshasurya02 <harshasurya02@gmail.com>
2025-09-02 01:02:30 +0530

    feat: Wrap header in Link component for navigation to Uptick page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,9 +27,11 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <div className="min-h-screen bg-gray-50 text-gray-900">
-          <header className="bg-white shadow-sm p-4">
-            <div className="max-w-6xl mx-auto">Uptick Demo</div>
-          </header>
+          <Link href={"/uptick"}>
+            <header className="bg-white shadow-sm p-4">
+              <div className="max-w-6xl mx-auto">Uptick Demo</div>
+            </header>
+          </Link>
           <div className="max-w-6xl mx-auto py-6">{children}</div>
         </div>
       </body>
